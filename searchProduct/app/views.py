9d52e4dea4d01a9e1621c01b2cb5c14a3dea1c5a
MiqueiasRihs <miqueias.rihs@gmail.com>
2020-07-16 23:06:50 -0300

    16-07

--- searchProduct/app/views.py
+++ searchProduct/app/views.py
@@ -10,35 +10,32 @@ import requests
 
 def show_index(request):
     obj_num = 0
+    allProducts = []
     resultJson = requests.get('https://www.obrazul.com.br/api/recruitment/products/')
     if(resultJson.status_code == 200):
         resultJson = json.loads(resultJson.content)
         products = resultJson["products"]
 
     for obj in products:
-        obj_num += 1
         objSelected = products[obj_num]
-        if(objSelected["name"] == "RIPA PRATA REFERÊNCIA 466"):
-            name = objSelected["name"]
-            price = objSelected["price"]
-            picture = objSelected["picture"]
-            price = objSelected["price"]
-            store = objSelected["store"]
-            storeName = store["name"]
-            storePhone = store["phone"]
-            storeLocation = store["location"]
+        store = objSelected["store"]
+        storeLocation = store["location"]
+        storeAddress = "Rua: " + storeLocation["address"] + ", " + "Nº " + storeLocation["address_number"] + ", Bairro: " + storeLocation["neighborhood"] + " " + storeLocation["city"] + "-" + storeLocation["state"]
 
-            storeAddress = "Rua: " + storeLocation["address"] + ", " + "Nº " + storeLocation["address_number"] + ", Bairro: " + storeLocation["neighborhood"] + storeLocation["city"] + "-" + storeLocation["state"]
+        eachProduct = {
+        'name' : objSelected["name"],
+        'price' : objSelected["price"],
+        'picture' : objSelected["picture"],
+        'price' : objSelected["price"],
+        'storeName' : store["name"],
+        'storePhone' : store["phone"],
+        'storeAddress' : storeAddress
+        }
 
-            print(name)
-            print(price)
-            print(picture)
-            print(price)
-            print(storeName)
-            print(storeAddress)
- 
-            break
-        else:
-            continue
+        allProducts.append(eachProduct)
 
-    return render(request, 'index.html', {'dictionary': resultJson})
+
+        obj_num += 1
+
+
+    return render(request, 'index.html', {'allProducts': allProducts})
