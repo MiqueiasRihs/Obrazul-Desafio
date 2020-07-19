809c8e40b809bdebf30e7b624e3ae80763017d14
MiqueiasRihs <miqueias.rihs@gmail.com>
2020-07-18 22:40:05 -0300

    18-07

--- searchProduct/app/views.py
+++ searchProduct/app/views.py
@@ -1,44 +1,99 @@
 from django.shortcuts import render, redirect
 import json
 import requests
-# from django.template.defaultfilters import register
+import re
 
-# @register.filter(name='dict_key')
-# def dict_key(d, k):
-#     '''Returns the given key from a dictionary.'''
-#     return d[k]
-
-def show_index(request):
-    prodprod = request.GET.get('searchField')
-    print(prodprod)
-    obj_num = 0
-    allProducts = []
+## Função para buscar os dados da url da Obrazul
+def bringProductList():
+    ## Realizando o GET e atribuindo o array
     resultJson = requests.get('https://www.obrazul.com.br/api/recruitment/products/')
     if(resultJson.status_code == 200):
         resultJson = json.loads(resultJson.content)
-        products = resultJson["products"]
+        listProducts = resultJson["products"]
     
-    for obj in products:
-        objSelected = products[obj_num]
-        if(str(prodprod) in objSelected["fullname"] or str(prodprod) in objSelected["name"]):
-            store = objSelected["store"]
+    return listProducts
+
+
+# -----------------------------------
+
+## Função para renderizar todos produtos
+def allProduct(request):
+
+    ## Definindo variaveis e arrays
+    objPosition = 0
+    allProducts = []
+
+    ## Buscando url e atribuindo ela a variavel
+    listProducts = bringProductList() 
+    
+    ## Percorredendo o Array de objetos e buscando os dados de acordo com o input do usuario
+    for obj in listProducts:
+        eachProduct = listProducts[objPosition]
+
+        store = eachProduct["store"]
+        storeLocation = store["location"]
+        storeAddress = "Rua: " + storeLocation["address"] + ", " + "Nº " + storeLocation["address_number"] + ", Bairro: " + storeLocation["neighborhood"] + " " + storeLocation["city"] + "-" + storeLocation["state"]
+
+        eachProduct = {
+        'name' : eachProduct["name"],
+        'price' : eachProduct["price"],
+        'picture' : eachProduct["picture"],
+        'price' : eachProduct["price"],
+        'storeName' : store["name"],
+        'storePhone' : store["phone"],
+        'storeAddress' : storeAddress
+        }
+
+        ## Nova lista com todos os produtos que correspondem com o input do usuario
+        allProducts.append(eachProduct)
+        objPosition += 1
+            
+    return render(request, 'allProducts.html', {'allProducts': allProducts})
+
+
+# -----------------------------------
+
+## Função para filtrar os produtos de acordo com o input do usuario
+def filteringProduct(request):
+
+    ## Definindo variaveis e arrays
+    objPosition = 0
+    searchResult = []
+
+    ## Pegando o valor inserido pelo usuario
+    userInput = request.GET.get('searchField', None)
+    print("Essa foi a pesquisa -->> ", userInput) 
+
+    ## Buscando url e atribuindo ela a variavel
+    listProducts = bringProductList() 
+    
+    ## Percorredendo o Array de objetos e buscando os dados de acordo com o input do usuario
+    for obj in listProducts:
+        selectedObject = listProducts[objPosition]
+
+        ## É verificado se o input do usuario corresponde com algo do fullname do produto ou com name da store e então o atribui a nova lista
+        if(re.search(str(userInput), selectedObject["fullname"], re.IGNORECASE) 
+            or re.search(str(userInput), selectedObject["store"]["name"], re.IGNORECASE)):
+
+            store = selectedObject["store"]
             storeLocation = store["location"]
             storeAddress = "Rua: " + storeLocation["address"] + ", " + "Nº " + storeLocation["address_number"] + ", Bairro: " + storeLocation["neighborhood"] + " " + storeLocation["city"] + "-" + storeLocation["state"]
 
             eachProduct = {
-            'name' : objSelected["name"],
-            'price' : objSelected["price"],
-            'picture' : objSelected["picture"],
-            'price' : objSelected["price"],
+            'name' : selectedObject["name"],
+            'price' : selectedObject["price"],
+            'picture' : selectedObject["picture"],
+            'price' : selectedObject["price"],
             'storeName' : store["name"],
             'storePhone' : store["phone"],
             'storeAddress' : storeAddress
             }
 
-            allProducts.append(eachProduct)
-            obj_num += 1
+            ## Nova lista com todos os produtos que correspondem com o input do usuario
+            searchResult.append(eachProduct)
+            objPosition += 1
 
         else:
-            obj_num += 1
-
-    return render(request, 'index.html', {'allProducts': allProducts})
+            objPosition += 1
+            
+    return render(request, 'searchResult.html', {'searchResult': searchResult})
--- searchProduct/searchProduct/urls.py
+++ searchProduct/searchProduct/urls.py
@@ -1,25 +1,11 @@
-"""searchProduct URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from app.views import *
+# allProduct
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('show_index/', show_index, name='show_index'),
-    path('show_index/<str:searchField>', show_index, name='show_index'),
+    path('', allProduct, name='allProduct'),
+    path('produto/', filteringProduct, name='filteringProduct'),
 
 ]
